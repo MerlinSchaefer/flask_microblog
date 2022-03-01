58f0ee9e709a31073df576f523665af168223e07
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2022-03-01 08:35:54 +0100

    add profile page and edit profile options

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,8 @@
 ## creates login forms
 
 from flask_wtf import FlaskForm
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import ValidationError, DataRequired, Email, EqualTo
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, TextAreaField
+from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Length
 from app.models import User
 
 
@@ -16,11 +16,11 @@ class LoginForm(FlaskForm):
 class RegistrationForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired()])
     email = StringField('Email', validators=[DataRequired(), Email()])
-    password = PasswordField('Password', validators=[DataRequired])
+    password = PasswordField('Password', validators=[DataRequired()])
     password2 = PasswordField('Repeat Password', 
                                 validators=[DataRequired(),
                                             EqualTo('password')])
-    submit = SubmitFiled('Register')
+    submit = SubmitField('Register')
 
     def validate_username(self, username):
         user = User.query.filter_by(username=username.data).first()
@@ -31,4 +31,8 @@ class RegistrationForm(FlaskForm):
         user = User.query.filter_by(email=email.data).first()
         if user is not None:
             raise ValidationError('Please use a different email address.')
-            
+
+class EditProfileForm(FlaskForm):
+    username = StringField('Username', validators=[DataRequired()])
+    about_me = TextAreaField('About Me', validators=[Length(min=0, max=280)])
+    submit = SubmitField('Submit')
