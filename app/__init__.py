d5dd15e7c05fc757c929f4fe6daf5a3a23d6bfbf
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2022-03-03 11:43:31 +0100

    add error handling and logging, fix username bug

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,9 @@ from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
-
+import logging
+from logging.handlers import RotatingFileHandler
+import os 
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -13,4 +15,20 @@ login = LoginManager(app)
 login.login_view = 'login'
 
 
-from app import routes, models
+from app import routes, models, errors
+
+
+if not app.debug:
+    ##possibly set up email logging 
+
+    if not os.path.exists('logs'):
+        os.mkdir('logs')
+    file_handler = RotatingFileHandler('logs/microblog.log', 
+                                        maxBytes=1024000, 
+                                        backupCount=10)
+    file_handler.setFormatter(logging.Formatter('%(asctime)s %(levelname)s: %(message)s [in %(pathname)s:%(lineno)d]'))
+    file_handler.setLevel(logging.INFO)
+    app.logger.addHandler(file_handler)
+
+    app.logger.setLevel(logging.INFO)
+    app.logger.info('Microblog startup')       
