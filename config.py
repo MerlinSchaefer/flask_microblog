fa1ade8fa1275498b94dff493a2207b20c45f515
MerlinSchaefer <48853130+MerlinSchaefer@users.noreply.github.com>
2022-03-16 11:37:13 +0100

    add password reset mail options

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
-
+from mail_config import default_user, default_pw
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 
@@ -10,3 +10,16 @@ class Config(object):
         'sqlite:///' + os.path.join(basedir,'app.db.sqlite')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     POSTS_PER_PAGE = 10
+    MAIL_SERVER = os.environ.get('MAIL_SERVER') or 'smtp.googlemail.com'
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 587)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') or 1 
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME') or default_user
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD') or default_pw
+    ADMINS = ['maschaefer713@gmail.com']
+
+## setup for gmail
+# set MAIL_SERVER=smtp.googlemail.com
+# set MAIL_PORT=587
+# set MAIL_USE_TLS=1
+# set MAIL_USERNAME=<your-gmail-username>
+# set MAIL_PASSWORD=<your-gmail-password>
